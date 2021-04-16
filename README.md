8ddc801192bf42d1daa6ce5c3d8f41c9f359b28a
ali <m.a.asadi@live.com>
2021-04-15 20:30:21 -0400

    up

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dicom_convertor
 A python script to convert **BMP** files to **DICOM**.
+
+
+## References
+1. https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3354356/
--- dicom_convertor.py
+++ dicom_convertor.py
@@ -8,6 +8,18 @@ import glob
 from PIL import Image
 from pydicom.data import get_testdata_file
 
+COUNTER = 0
+
+def update_destination_file_name (file_name):
+	"""
+	Update destination file_name with adding `COUNTER` to the beginning 
+	"""
+	global COUNTER 
+	COUNTER += 1
+	splited = file_name.split('/')
+	return file_name[:len(file_name)-len(splited[-1])] + 'Image%05d' % COUNTER +'_'+splited[-1]
+
+
 def convert_to_dicom(file_name):
 	"""
 	Convert bmp to dicom using pydicom 
@@ -17,22 +29,24 @@ def convert_to_dicom(file_name):
 	img = Image.open(file_name+".bmp")
 	npa = np.asarray(img)
 	ds.PixelData = img.tobytes()
-	ds.save_as(file_name+".dcm")
-	print("DONE\t "+file_name+".dcm")
+	name = update_destination_file_name(file_name)
+	ds.save_as(name+'.dcm')
+	print("DONE\t "+name+".dcm")
 	
 def img2dcm_from_bmp(file_name):
 	"""
 	Convert bmp to dicom using img2dcm from dcmtl 
 	To install: brew install dcmtk
 	"""
-	os.system('img2dcm -i BMP '+file_name+'.bmp '+file_name+'.dcm ')	
-	print("DONE\t "+file_name+".dcm")
+	name = update_destination_file_name(file_name)
+	os.system('img2dcm -i BMP '+file_name+'.bmp '+name+'.dcm ')	
+	print("DONE\t "+name+".dcm")
 
 def get_file_names(DIR):
 	"""
 	Get name of bmp files in DIR
 	"""
-	return glob.glob(DIR+"*.bmp")
+	return sorted(glob.glob(DIR+"*.bmp"))
 
 if __name__ == '__main__':
 	if len(sys.argv) != 3 or sys.argv[1] in ["--help", "-h"]:
@@ -44,7 +58,6 @@ if __name__ == '__main__':
 		DIR = DIR+"/"
 
 	file_names = get_file_names(DIR)
-	
 	if int(sys.argv[1]) == 0:
 		print("RUN\t pydicom...")
 		for name in file_names:
@@ -53,3 +66,5 @@ if __name__ == '__main__':
 		print("RUN\t dcmtl...")
 		for name in file_names:
 			img2dcm_from_bmp(name[:-4])
+
+	# print(COUNTER)
